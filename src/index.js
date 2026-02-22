269fc399dd6a973002d8621145f627d9fae12e53
carey-ui <careyf30@gmail.com>
2026-02-22 15:02:42 +0000

    moved Forecast HTML to JS Function

--- src/index.js
+++ src/index.js
@@ -30,10 +30,30 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs"];
+  let forecastHTML = "";
+
+  days.forEach(function (day) {
+    forecastHTML += `<div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">⛅</div>
+          <div class="weather-forecast-temperatures">
+            <div class="highlow-temp"><strong>19°</strong></div>
+            <div class="highlow-temp">9°</div>
+          </div>
+        </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Dublin");
+displayForecast();
 
 let now = new Date();
 
