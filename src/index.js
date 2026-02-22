74383bd6534c4ebc3c83d4764ddf5b3cbea62e0b
carey-ui <careyf30@gmail.com>
2026-02-22 16:19:22 +0000

    Added Day, Time to Forecast JS

--- src/index.js
+++ src/index.js
@@ -31,6 +31,21 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return day[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "acbod87f7ctb68340e47b7b3abe9ae79";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
@@ -38,19 +53,19 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-  let days = ["Mon", "Tues", "Wed", "Thurs", "Fri"];
   let forecastHTML = "";
 
-  days.forEach(function (day) {
-    forecastHTML += `<div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">⛅</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML += `<div class="weather-forecast-day">
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <div><img src="${day.condition.icon_url}" class="weather-forecast-icon"></div>
           <div class="weather-forecast-temperatures">
-            <div class="highlow-temp"><strong>19°</strong></div>
-            <div class="highlow-temp">9°</div>
+            <div class="highlow-temp"><strong>${Math.round(day.temperature.maximum)}°</strong></div>
+            <div class="highlow-temp">${Math.round(day.temperature.minimum)}°</div>
           </div>
         </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
